9204e5536624703646a8885f278bb80e92b7e3c4
Richard Yance <yance0022@gmail.com>
2019-05-08 20:28:48 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -3,6 +3,7 @@ class CreateActors < ActiveRecord::Migration[5.2]
     create_table :actors do |t|
       t.string :first_name
       t.string :last_name
+      t.integer :actor_id
     end
   end
 end
